ea8ce798107f76365635b0d66173297cc03b292e
Scott <scott@scotts-mbp.lan>
2023-05-24 11:32:08 -0700

    added projects to homepage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,16 +1,28 @@
+import ProjectCard from "@/components/ui/ProjectCard";
 import Skills from "@/components/ui/Skills";
 import Hero from "@/components/ui/layout/Hero";
 import PageContainer from "@/components/ui/layout/PageContainer";
 import PageHero from "@/components/ui/layout/PageHero";
 import PageWrapper from "@/components/ui/layout/PageWrapper";
+import { getProjects } from "@/sanity/sanity-utils";
 
-export default function Home() {
+export default async function Home() {
+  const projectsData = await getProjects();
+
   return (
     <PageWrapper>
       <Hero />
-      <PageContainer>
+      <PageContainer className="container mb-16">
         {/* @ts-expect-error Server Component */}
         <Skills />
+        <div className="py-8 space-y-4">
+          <h1 className="big-title">Projects</h1>
+        </div>
+        <div className="grid md:grid-cols-2 lg:gap-24 md:gap-12 gap-8 justify-center">
+          {projectsData.map((project) => {
+            return <ProjectCard key={project._id} project={project} />;
+          })}
+        </div>
       </PageContainer>
     </PageWrapper>
   );
